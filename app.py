4f05fad5b34d6156eb711b0bf98fb77265fcb933
DickyfliPerdanaPutra <dicky@inixindojogja.co.id>
2025-08-28 10:20:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Selamat datang di Inixindo Jogja!"
+    return "Selamat datang di Inixindo Jogja! terkecuali Readme"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
